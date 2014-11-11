d12bf430489c02c17546f8d0e25342cc92e23d66
Elliot Hursh <elliothursh@gmail.com>
2014-11-11 10:54:41 -0600

    fix-ish for restart failures

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,5 @@ default[:sidekiq][:environment] = "production"
 default[:sidekiq][:config_file] = "config/sidekiq.yml"
 default[:sidekiq][:pids_dir] = "tmp/pids"
 default[:sidekiq][:sidekiq_conf] = "/etc/sidekiq.conf"
+default[:sidekiq][:deadline_timeout] = 60
 default[:sidekiq][:user] = "root"
