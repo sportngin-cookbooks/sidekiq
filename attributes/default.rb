f348197641e09588be2f8f9ec3d3d318fc1e33f7
Nick LaMuro <nicklamuro@gmail.com>
2014-12-16 13:22:15 -0600

    Make upstart optional

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,8 @@
-default[:sidekiq][:exec_prefix] = "exec"
-default[:sidekiq][:environment] = "production"
 default[:sidekiq][:config_file] = "config/sidekiq.yml"
+default[:sidekiq][:deadline_timeout] = 60
+default[:sidekiq][:environment] = "production"
+default[:sidekiq][:exec_prefix] = "exec"
 default[:sidekiq][:pids_dir] = "tmp/pids"
 default[:sidekiq][:sidekiq_conf] = "/etc/sidekiq.conf"
-default[:sidekiq][:deadline_timeout] = 60
+default[:sidekiq][:use_upstart] = false
 default[:sidekiq][:user] = "root"
--- recipes/service.rb
+++ recipes/service.rb
@@ -1,9 +1,15 @@
-service "sidekiq" do
-  provider Chef::Provider::Service::Upstart
-  action :enable
-end
+if node[:sidekiq][:use_upstart]
+  service "sidekiq" do
+    provider Chef::Provider::Service::Upstart
+    action :enable
+  end
 
-service "sidekiq-manager" do
-  provider Chef::Provider::Service::Upstart
-  action :enable
+  service "sidekiq-manager" do
+    provider Chef::Provider::Service::Upstart
+    action :enable
+  end
+else
+  service "sidekiq" do
+    action :nothing
+  end
 end
