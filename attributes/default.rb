e76e68f5c627941eda0b9342d3b50d9a122167d7
Andy Fleener <andrew.fleener@sportngin.com>
2015-01-02 10:18:54 -0600

    Add logging to sidekiq

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,9 @@
 default[:sidekiq][:exec_prefix] = "exec"
 default[:sidekiq][:environment] = "production"
 default[:sidekiq][:config_file] = "config/sidekiq.yml"
+default[:sidekiq][:logfile] = "config/sidekiq.yml"
 default[:sidekiq][:pids_dir] = "tmp/pids"
+default[:sidekiq][:log_dir] = "log"
 default[:sidekiq][:sidekiq_conf] = "/etc/sidekiq.conf"
 default[:sidekiq][:deadline_timeout] = 60
 default[:sidekiq][:user] = "root"
